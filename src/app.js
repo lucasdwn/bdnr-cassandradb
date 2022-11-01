b3b13e6ed39b043d3a1d28c692f2f100ae2d0baa
lucasdwn <lfcosta0804@gmail.com>
2022-10-31 21:21:41 -0300

    crud de usuarios finalizados

--- src/app.js
+++ src/app.js
@@ -17,21 +17,100 @@ const client = new Client({
         secureConnectBundle: "C:\\Users\\ddaiw\\Desktop\\cassandra\\secure-connect-fateccassandra.zip",
     },
     credentials: {
-        username: "",
-        password: "",
+        username: "erqchagCaJppsujYWbGvJtZj",
+        password: "dwNO65hOjWHGWBooffJEGuR+xqXg_pLLxq-,BB9CvdzII_l.tqj23iPf0J4DLv4rCKIW,7.nBxhHWtqOqJzncXNGyNmG+Uimh_Dp.-i4W5W3R98dSz+pn1xHuG_UguU9",
     },
 });
 Start();
-function Listar(rs) {
+// Usuarios
+function CadastrarUsuario() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let email = prompt("Insira um E-mail Válido: ");
+        let nome = prompt("Insira seu Nome: ");
+        let sobrenome = prompt("Insira seu Sobrenome: ");
+        let cpf = prompt("Insira seu CPF: ");
+        let end = prompt("Insira seu endereço: ");
+        yield client.connect();
+        yield client.execute("use mercadolivre");
+        let validacao = yield client.execute(`SELECT * FROM usuario WHERE Email = '${email}'`);
+        if (validacao.rows.length == 0) {
+            yield client.execute(`INSERT INTO usuario(email, nome, sobrenome, cpf, end) VALUES('${email}','${nome}','${sobrenome}','${cpf}','${end}')`);
+            console.log("Usuario cadastrado com sucesso!");
+            let opcao = Number(prompt("Deseja cadastrar novamente? 1-Sim 2-Não: "));
+            switch (opcao) {
+                case 1:
+                    CadastrarUsuario();
+                    break;
+                case 2:
+                    console.clear();
+                    Usuarios();
+                    break;
+            }
+        }
+        else {
+            console.clear();
+            console.log('Email já cadastrado');
+            Usuarios();
+        }
+    });
+}
+function EditarUsuario() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let opcaoEmail = prompt("Digite o Email do usuario que deseja editar: ");
+        yield client.connect();
+        yield client.execute("use mercadolivre");
+        let validacao = yield client.execute(`SELECT * FROM usuario WHERE Email = '${opcaoEmail}'`);
+        if (validacao.rows.length == 0) {
+            console.clear();
+            console.log('Usuario não existe');
+            Usuarios();
+        }
+        else {
+            let novoNome = prompt(`Nome Atual: ${validacao.rows[0].nome}, Digite o Novo nome: `);
+            let novoSobrenome = prompt(`Sobrenome Atual: ${validacao.rows[0].sobrenome}, Digite o Novo sobrenome: `);
+            let novoCPF = prompt(`CPF Atual: ${validacao.rows[0].cpf}, Digite o Novo cpf: `);
+            let novoEnd = prompt(`Endereço Atual: ${validacao.rows[0].cpf}, Digite o Novo endereço: `);
+            if (novoNome == '' && novoSobrenome == '' && novoCPF == '' && novoEnd == '') {
+                console.log('Preencha todos os campos por favor!');
+                Usuarios();
+            }
+            else {
+                yield client.execute(`UPDATE usuario SET nome = '${novoNome}', sobrenome = '${novoSobrenome}', cpf = '${novoCPF}', end = '${novoEnd}' WHERE email = '${opcaoEmail}'`);
+                console.log("Usuario Editado com sucesso");
+                Usuarios();
+            }
+        }
+    });
+}
+function ListarUsuarios() {
     return __awaiter(this, void 0, void 0, function* () {
         yield client.connect();
         yield client.execute("use mercadolivre");
-        let ts = yield client.execute(rs);
-        let linhas = ts.rows;
-        for (var i = 0; i < linhas.length; i++) {
-            console.log(JSON.stringify(linhas[i]));
+        let query = yield client.execute("SELECT * from usuario;");
+        let queryResult = query.rows;
+        for (var i = 0; i < queryResult.length; i++) {
+            console.log(`----------------------`);
+            console.log(`Email: ${queryResult[i].email} | Nome: ${queryResult[i].nome} | Sobrenome: ${queryResult[i].sobrenome} | CPF: ${queryResult[i].cpf} | Endereço: ${queryResult[i].end}`);
         }
-        yield client.shutdown();
+        Usuarios();
+    });
+}
+function ExcluirUsuario() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let opcaoEmail = prompt("Digite o Email do usuario que deseja excluir: ");
+        yield client.connect();
+        yield client.execute("use mercadolivre");
+        let validacao = yield client.execute(`SELECT * FROM usuario WHERE Email = '${opcaoEmail}'`);
+        if (validacao.rows.length == 0) {
+            console.clear();
+            console.log('Usuario não existe');
+            Usuarios();
+        }
+        else {
+            yield client.execute(`DELETE FROM usuario WHERE email = '${opcaoEmail}'`);
+            console.log("Usuario foi excluido com sucesso");
+            Usuarios();
+        }
     });
 }
 function Start() {
@@ -51,46 +130,68 @@ function Start() {
     Switch(opcao);
 }
 function Switch(opcao) {
-    switch (opcao) {
-        case 1:
-            Usuarios();
-            break;
-        case 2:
-            Vendedor();
-            break;
-        case 3:
-            Produto();
-            break;
-        case 4:
-            Compra();
-            break;
-        case 0:
-            console.log('Até logo!');
-            console.clear();
-            break;
-    }
+    return __awaiter(this, void 0, void 0, function* () {
+        switch (opcao) {
+            case 1:
+                console.clear();
+                Usuarios();
+                break;
+            case 2:
+                console.clear();
+                Vendedor();
+                break;
+            case 3:
+                console.clear();
+                Produto();
+                break;
+            case 4:
+                console.clear();
+                Compra();
+                break;
+            case 0:
+                console.log('Até logo!');
+                console.clear();
+                yield client.shutdown();
+                break;
+        }
+    });
 }
 function Usuarios() {
-    console.log(`****************************`);
-    console.log(`| Opções para Usuario:`);
-    console.log(`----------------------`);
-    console.log(`| 1 - Cadastrar Usuario`);
-    console.log(`| 2 - Editar Usuario`);
-    console.log(`| 3 - Listar Usuarios`);
-    console.log(`| 4 - Excluir Usuario`);
-    console.log(`****************************`);
-    console.log(`| 0 - Sair`);
-    let opcao = Number(prompt("Insire a opção desejada: "));
-    switch (opcao) {
-        case 3:
-            const rs = ("SELECT * from usuario;");
-            Listar(rs);
-            break;
-        case 0:
-            console.log('Até logo!');
-            console.clear();
-            break;
-    }
+    return __awaiter(this, void 0, void 0, function* () {
+        console.log(`****************************`);
+        console.log(`| Opções para Usuario:`);
+        console.log(`----------------------`);
+        console.log(`| 1 - Cadastrar Usuario`);
+        console.log(`| 2 - Editar Usuario`);
+        console.log(`| 3 - Listar Usuarios`);
+        console.log(`| 4 - Excluir Usuario`);
+        console.log(`****************************`);
+        console.log(`| 5 - Voltar`);
+        console.log(`| 0 - Sair`);
+        let opcao = Number(prompt("Insire a opção desejada: "));
+        switch (opcao) {
+            case 1:
+                CadastrarUsuario();
+                break;
+            case 2:
+                EditarUsuario();
+                break;
+            case 3:
+                ListarUsuarios();
+                break;
+            case 4:
+                ExcluirUsuario();
+                break;
+            case 5:
+                console.clear();
+                Start();
+            case 0:
+                console.log('Até logo!');
+                console.clear();
+                yield client.shutdown();
+                break;
+        }
+    });
 }
 function Vendedor() {
     console.log(`****************************`);
@@ -101,6 +202,7 @@ function Vendedor() {
     console.log(`| 3 - Listar Vendedores`);
     console.log(`| 4 - Excluir Vendedor`);
     console.log(`****************************`);
+    console.log(`| 5 - Voltar`);
     console.log(`| 0 - Sair`);
     let opcao = Number(prompt("Insire a opção desejada: "));
 }
@@ -113,6 +215,7 @@ function Produto() {
     console.log(`| 3 - Listar Produtos`);
     console.log(`| 4 - Excluir Produto`);
     console.log(`****************************`);
+    console.log(`| 5 - Voltar`);
     console.log(`| 0 - Sair`);
     let opcao = Number(prompt("Insire a opção desejada: "));
 }
@@ -125,6 +228,7 @@ function Compra() {
     console.log(`| 3 - Listar Compra`);
     console.log(`| 4 - Excluir Compra`);
     console.log(`****************************`);
+    console.log(`| 5 - Voltar`);
     console.log(`| 0 - Sair`);
     let opcao = Number(prompt("Insire a opção desejada: "));
 }
--- src/app.ts
+++ src/app.ts
@@ -7,24 +7,123 @@ const client = new Client({
       secureConnectBundle: "C:\\Users\\ddaiw\\Desktop\\cassandra\\secure-connect-fateccassandra.zip",
    },
    credentials: {
-      username: "",
-      password: "",
+      username: "erqchagCaJppsujYWbGvJtZj",
+      password: "dwNO65hOjWHGWBooffJEGuR+xqXg_pLLxq-,BB9CvdzII_l.tqj23iPf0J4DLv4rCKIW,7.nBxhHWtqOqJzncXNGyNmG+Uimh_Dp.-i4W5W3R98dSz+pn1xHuG_UguU9",
    },
 });
 
 Start();
 
-async function Listar(rs: string) {
+// Usuarios
+
+async function CadastrarUsuario() {
+
+   let email = prompt("Insira um E-mail Válido: ")
+   let nome = prompt("Insira seu Nome: ")
+   let sobrenome = prompt("Insira seu Sobrenome: ")
+   let cpf = prompt("Insira seu CPF: ")
+   let end = prompt("Insira seu endereço: ")
+
    await client.connect();
    await client.execute("use mercadolivre");
-   let ts = await client.execute(rs)
-   let linhas = ts.rows;
 
-   for (var i = 0; i < linhas.length; i++) {
-      console.log(JSON.stringify(linhas[i]))
+   let validacao = await client.execute(`SELECT * FROM usuario WHERE Email = '${email}'`);
+
+   if (validacao.rows.length == 0) {
+      await client.execute(`INSERT INTO usuario(email, nome, sobrenome, cpf, end) VALUES('${email}','${nome}','${sobrenome}','${cpf}','${end}')`);
+
+      console.log("Usuario cadastrado com sucesso!");
+
+      let opcao = Number(prompt("Deseja cadastrar novamente? 1-Sim 2-Não: "))
+
+      switch (opcao) {
+         case 1:
+            CadastrarUsuario();
+            break
+         case 2:
+            console.clear();
+            Usuarios();
+            break
+      }
+
    }
 
-   await client.shutdown();
+   else {
+      console.clear();
+      console.log('Email já cadastrado')
+      Usuarios();
+   }
+
+
+}
+
+async function EditarUsuario() {
+   let opcaoEmail = prompt("Digite o Email do usuario que deseja editar: ")
+
+   await client.connect();
+   await client.execute("use mercadolivre");
+
+
+   let validacao = await client.execute(`SELECT * FROM usuario WHERE Email = '${opcaoEmail}'`);
+
+   if (validacao.rows.length == 0) {
+      console.clear();
+      console.log('Usuario não existe')
+      Usuarios();
+   }
+
+   else {
+      let novoNome = prompt(`Nome Atual: ${validacao.rows[0].nome}, Digite o Novo nome: `);
+      let novoSobrenome = prompt(`Sobrenome Atual: ${validacao.rows[0].sobrenome}, Digite o Novo sobrenome: `);
+      let novoCPF = prompt(`CPF Atual: ${validacao.rows[0].cpf}, Digite o Novo cpf: `)
+      let novoEnd = prompt(`Endereço Atual: ${validacao.rows[0].cpf}, Digite o Novo endereço: `)
+
+      if (novoNome == '' && novoSobrenome == '' && novoCPF == '' && novoEnd == '') {
+         console.log('Preencha todos os campos por favor!')
+         Usuarios();
+      }
+      else {
+         await client.execute(`UPDATE usuario SET nome = '${novoNome}', sobrenome = '${novoSobrenome}', cpf = '${novoCPF}', end = '${novoEnd}' WHERE email = '${opcaoEmail}'`)
+         console.log("Usuario Editado com sucesso");
+         Usuarios();
+      }
+   }
+
+}
+
+async function ListarUsuarios() {
+   await client.connect();
+   await client.execute("use mercadolivre");
+   let query = await client.execute("SELECT * from usuario;");
+   let queryResult = query.rows;
+
+   for (var i = 0; i < queryResult.length; i++) {
+      console.log(`----------------------`)
+      console.log(`Email: ${queryResult[i].email} | Nome: ${queryResult[i].nome} | Sobrenome: ${queryResult[i].sobrenome} | CPF: ${queryResult[i].cpf} | Endereço: ${queryResult[i].end}`)
+   }
+
+   Usuarios()
+}
+
+async function ExcluirUsuario() {
+   let opcaoEmail = prompt("Digite o Email do usuario que deseja excluir: ")
+
+   await client.connect();
+   await client.execute("use mercadolivre");
+
+   let validacao = await client.execute(`SELECT * FROM usuario WHERE Email = '${opcaoEmail}'`);
+
+   if (validacao.rows.length == 0) {
+      console.clear();
+      console.log('Usuario não existe')
+      Usuarios();
+   }
+   else {
+      await client.execute(`DELETE FROM usuario WHERE email = '${opcaoEmail}'`)
+      console.log("Usuario foi excluido com sucesso");
+      Usuarios();
+   }
+
 }
 
 function Start() {
@@ -45,28 +144,33 @@ function Start() {
    Switch(opcao)
 }
 
-function Switch(opcao: number) {
+async function Switch(opcao: number) {
    switch (opcao) {
       case 1:
+         console.clear();
          Usuarios();
          break;
       case 2:
+         console.clear();
          Vendedor();
          break;
       case 3:
+         console.clear();
          Produto();
          break;
       case 4:
+         console.clear();
          Compra();
          break;
       case 0:
          console.log('Até logo!');
          console.clear();
+         await client.shutdown();
          break;
    }
 }
 
-function Usuarios() {
+async function Usuarios() {
    console.log(`****************************`)
    console.log(`| Opções para Usuario:`)
    console.log(`----------------------`)
@@ -75,16 +179,29 @@ function Usuarios() {
    console.log(`| 3 - Listar Usuarios`)
    console.log(`| 4 - Excluir Usuario`)
    console.log(`****************************`)
+   console.log(`| 5 - Voltar`)
    console.log(`| 0 - Sair`)
    let opcao = Number(prompt("Insire a opção desejada: "))
    switch (opcao) {
-      case 3:
-         const rs = ("SELECT * from usuario;");
-         Listar(rs);
+      case 1:
+         CadastrarUsuario();
          break
+      case 2:
+         EditarUsuario();
+         break
+      case 3:
+         ListarUsuarios();
+         break
+      case 4:
+         ExcluirUsuario();
+         break
+      case 5:
+         console.clear();
+         Start();
       case 0:
-         console.log('Até logo!')
-         console.clear()
+         console.log('Até logo!');
+         console.clear();
+         await client.shutdown();
          break
    }
 }
@@ -98,6 +215,7 @@ function Vendedor() {
    console.log(`| 3 - Listar Vendedores`)
    console.log(`| 4 - Excluir Vendedor`)
    console.log(`****************************`)
+   console.log(`| 5 - Voltar`)
    console.log(`| 0 - Sair`)
    let opcao = Number(prompt("Insire a opção desejada: "))
 }
@@ -111,6 +229,7 @@ function Produto() {
    console.log(`| 3 - Listar Produtos`)
    console.log(`| 4 - Excluir Produto`)
    console.log(`****************************`)
+   console.log(`| 5 - Voltar`)
    console.log(`| 0 - Sair`)
    let opcao = Number(prompt("Insire a opção desejada: "))
 }
@@ -124,6 +243,7 @@ function Compra() {
    console.log(`| 3 - Listar Compra`)
    console.log(`| 4 - Excluir Compra`)
    console.log(`****************************`)
+   console.log(`| 5 - Voltar`)
    console.log(`| 0 - Sair`)
    let opcao = Number(prompt("Insire a opção desejada: "))
 }
